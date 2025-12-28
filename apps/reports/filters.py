3353306bc1a143553e245506ed2b626e731fcc36
Grigorii Grachev <grigory.grachev@gmail.com>
2025-12-28 21:22:23 +0300

    upd custom export

--- apps/reports/filters.py
+++ apps/reports/filters.py
@@ -12,7 +12,14 @@ class PolicyExportFilter(django_filters.FilterSet):
         field_name="dfa_number", lookup_expr="icontains", label="Номер ДФА"
     )
     client__client_name = django_filters.CharFilter(
-        field_name="client__client_name", lookup_expr="icontains", label="Клиент"
+        field_name="client__client_name",
+        lookup_expr="icontains",
+        label="Лизингополучатель",
+    )
+    policyholder__client_name = django_filters.CharFilter(
+        field_name="policyholder__client_name",
+        lookup_expr="icontains",
+        label="Страхователь",
     )
     start_date_from = django_filters.DateFilter(
         field_name="start_date", lookup_expr="gte", label="Дата начала от"
@@ -50,13 +57,34 @@ class PaymentExportFilter(django_filters.FilterSet):
     policy__client__client_name = django_filters.CharFilter(
         field_name="policy__client__client_name",
         lookup_expr="icontains",
-        label="Клиент",
+        label="Лизингополучатель",
+    )
+    policy__policyholder__client_name = django_filters.CharFilter(
+        field_name="policy__policyholder__client_name",
+        lookup_expr="icontains",
+        label="Страхователь",
     )
     due_date_from = django_filters.DateFilter(
         field_name="due_date", lookup_expr="gte", label="Дата платежа от"
     )
     due_date_to = django_filters.DateFilter(
         field_name="due_date", lookup_expr="lte", label="Дата платежа до"
+    )
+    paid_date_from = django_filters.DateFilter(
+        field_name="paid_date", lookup_expr="gte", label="Дата факт. оплаты от"
+    )
+    paid_date_to = django_filters.DateFilter(
+        field_name="paid_date", lookup_expr="lte", label="Дата факт. оплаты до"
+    )
+    insurer_date_from = django_filters.DateFilter(
+        field_name="insurer_date",
+        lookup_expr="gte",
+        label="Дата согласования акта с СК от",
+    )
+    insurer_date_to = django_filters.DateFilter(
+        field_name="insurer_date",
+        lookup_expr="lte",
+        label="Дата согласования акта с СК до",
     )
     is_paid = django_filters.BooleanFilter(
         field_name="paid_date", lookup_expr="isnull", exclude=True, label="Оплачен"
